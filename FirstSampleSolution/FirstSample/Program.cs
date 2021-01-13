735b7ec016f9233d6b3207b82083a10a57a3791e
zzhao28 <77073248+zzhao28@users.noreply.github.com>
2021-01-13 10:15:56 -0700

    create first sample console program

---
+++ FirstSampleSolution/FirstSample/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace FirstSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // the WriteLine() command will output the
+            //    the to the console window.
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("This course section is:\tOA03");
+            Console.WriteLine("Your instructor name is:\t Don Welch");
+        }
+    }
+}
