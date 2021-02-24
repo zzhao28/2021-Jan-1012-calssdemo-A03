eaa509978f49433d390fe4e59162fc07e98acde1
zzhao28 <77073248+zzhao28@users.noreply.github.com>
2021-02-24 10:01:55 -0700

    Update Program.cs

--- FileIOSolution/FileIO/Program.cs
+++ FileIOSolution/FileIO/Program.cs
@@ -4,6 +4,9 @@ namespace FileIO
 {
     class Program
     {
+        //Main() is a method
+        // special method
+        // entry point into your program execution
         static void Main(string[] args)
         {
             /*
@@ -29,19 +32,18 @@ namespace FileIO
             //post loop structure, used to control menu
             do
             {
-
-
                 Console.WriteLine("File I/O options:");
                 Console.WriteLine("a) Hard-coded file name.");
                 Console.WriteLine("b) Using Windows Environment (Desktop, Documents, Download) path file name.");
                 Console.WriteLine("c) Using Openfile dialog to obtain file name.");
                 Console.WriteLine("x) Exit.\n");
-                Console.Write("Enter the menu option for File I/O");
+                Console.Write("Enter the menu option for File I/O:\t");
                 inputTemp = Console.ReadLine();
                 switch(inputTemp.ToUpper())
                 {
                     case "A":
                         {
+
                             break;
                         }
                     case "B":
@@ -62,15 +64,70 @@ namespace FileIO
                             Console.WriteLine($"{inputTemp} is not a valid menu option. Try again.");
                             break;
                         }
-
-
-
                 }
 
             } while (inputTemp.ToUpper() != "X");
-            
+             
+        }
+        /*  Methods
+         * 
+         * Why????
+         *      reduce code redundancy
+         *      break up your code into smaller managable pieces (modularization)
+         *      
+         * Where do methods go?
+         *      methods go into your program class
+         *      
+         * Syntax for a method
+         *     accesstype returndatatype MethodName([list of parameters])
+         *     {
+         *         coding block
+         *     }
+         *     
+         *     accesstype?
+         *     This is dependant on what is the Main access type or if the
+         *          class is NOT the entry point to your entire application,
+         *          could be public, private, internal, protected,..
+         *          
+         *     returndatatype
+         *     Methods have been called by many names
+         *     Methods have been called subroutines, functions, methods
+         *     Subroutines and functions are different in that subroutines return
+         *          no data to the calling statement; whereass functions will return
+         *          data to the calling statement.
+         *     the return datatype must be a valid language datatype
+         *     the return datatype in C# that returns nothing is the keyword void
+         *     
+         *     MethodName
+         *     This is what the method is called
+         *     The method name is usually unique
+         *     The method names that are not uniques are paired with their list of
+         *         parameters to create a method signature. The method signature
+         *         MUST be unique. This is referred to as overload methods.
+         *          
+         *     [list of parameters]     
+         *     What is a parameter?
+         *     datatype parametername
+         *     consider the parameter a variable within your method that has
+         *          already been declare and can be used as other variables
+         *     datatype parametername
+         *     
+         *     What is a list of parameters?
+         *     datatype parametername, datatype parametername, .....
+         */
+        static string HardCodedFileName()
+        {
+            //setup a path name to the folder on your machine that contains
+            //   the file to be read
+            string Folder_Pathname = @"F:\GitHub\CPSC-1012\FileProcessing\";
 
-            
+            //concatenate a file name to the pathname
+            string Full_Path_FileName;
+            Full_Path_FileName = Folder_Pathname + @"OneColumn.txt";
+
+            //BECAUSE the method indicates a returned datatype of string (anything
+            //      but void), the method REQUIRES a return xxxx; statement
+            return Full_Path_FileName;
         }
     }
 }
